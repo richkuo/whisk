74e90886e46492acb6d409b4aa3368949706766e
Richard Kuo <richard.p.kuo@gmail.com>
2019-01-31 18:26:18 -0500

    Adding users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # resources :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+      resources :users
+    end
+
+    unauthenticated do
+      root 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
